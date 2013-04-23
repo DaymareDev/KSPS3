d20c922e772403fd756929e9eb6fa155f67706cf
Marius Brendmoe <mariusbrendmoe@hotmail.com>
2013-04-23 13:33:21 +0200

    Now possible to use GUI to select save file (It is loaded into a viewer if found)

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,6 +1,9 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 
+#include <QFileDialog>
+#include <QTextEdit>
+
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow)
@@ -11,4 +14,24 @@ MainWindow::MainWindow(QWidget *parent) :
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+
+void MainWindow::on_BrowseSaveButton_clicked()
+{
+    QTextEdit* pathField = this->findChild<QTextEdit*>("SavePathTextEdit");
+    QString path = pathField->toPlainText();
+    QFile selectedDir(path);
+    path = QFileDialog::getOpenFileName(0, QString("Select the save file to use"), path);
+    selectedDir.setFileName(path);
+    if(selectedDir.exists())
+    {
+        pathField->setText(path);
+        if(path.endsWith(".sfs") && selectedDir.open(QIODevice::ReadOnly))
+        {
+            QTextEdit* fileEditor = this->findChild<QTextEdit*>("saveFileTextEdit");
+            fileEditor->setText(QString(selectedDir.readAll()));
+
+        }
+    }
 }
